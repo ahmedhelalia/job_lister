10cea79df3b1bf2d7c8ac757200c2cf9d99b0099
ahmed helalia <ahmedhelalia213@gmial.com>
2024-11-19 14:57:35 +0200

    simple validation for user input

--- create.php
+++ create.php
@@ -3,25 +3,29 @@ include_once 'config/init.php';
 ?>
 <?php
 $job = new Job;
-if(isset($_POST['submit'])){
-     // Create Data array
-     $data = array();
-     $data['job_title'] = $_POST['job_title'];
-     $data['company'] = $_POST['company'];
-     $data['category_id'] = $_POST['category'];
-     $data['description'] = $_POST['description'];
-     $data['location'] = $_POST['location'];
-     $data['salary'] = $_POST['salary'];
-     $data['contact_email'] = $_POST['contact_email'];
-     $data['contact_user'] = $_POST['contact_user'];
-
-     if($job->create($data)){
-        redirect('index.php',"Your job has been listed",'success');
-     }else{
-        redirect('index.php',"Something Went Wrong",'error');
-
-     }
- 
+if (isset($_POST['submit'])) {
+   // Create Data array
+   $data = array();
+   $data['job_title'] = $_POST['job_title'];
+   $data['company'] = $_POST['company'];
+   $data['category_id'] = $_POST['category'];
+   $data['description'] = $_POST['description'];
+   $data['location'] = $_POST['location'];
+   $data['salary'] = $_POST['salary'];
+   $data['contact_email'] = $_POST['contact_email'];
+   $data['contact_user'] = $_POST['contact_user'];
+   foreach ($data as $key => $value) {
+      if (empty($value) || $value == 0) {
+         redirect('create.php', "Please Fill all fields and choose a category", 'error');
+      }else{
+         addslashes($value);
+      }
+   }
+   if ($job->create($data)) {
+      redirect('index.php', "Your job has been listed", 'success');
+   } else {
+      redirect('index.php', "Something Went Wrong", 'error');
+   }
 }
 $template = new Template('templates/job-create.php');
 $template->categories = $job->getCategories();
--- edit.php
+++ edit.php
@@ -3,26 +3,31 @@ include_once 'config/init.php';
 ?>
 <?php
 $job = new Job;
-$job_id = isset($_GET['id']) ? $_GET['id'] : null ;
-if(isset($_POST['submit'])){
-     // Create Data array
-     $data = array();
-     $data['job_title'] = $_POST['job_title'];
-     $data['company'] = $_POST['company'];
-     $data['category_id'] = $_POST['category'];
-     $data['description'] = $_POST['description'];
-     $data['location'] = $_POST['location'];
-     $data['salary'] = $_POST['salary'];
-     $data['contact_email'] = $_POST['contact_email'];
-     $data['contact_user'] = $_POST['contact_user'];
-
-     if($job->update($job_id,$data)){
-        redirect('index.php',"Your job has been Updated",'success');
-     }else{
-        redirect('index.php',"Something Went Wrong",'error');
-
-     }
- 
+$job_id = isset($_GET['id']) ? $_GET['id'] : null;
+if (isset($_POST['submit'])) {
+   // Create Data array
+   $data = array();
+   $data['job_title'] = $_POST['job_title'];
+   $data['company'] = $_POST['company'];
+   $data['category_id'] = $_POST['category'];
+   $data['description'] = $_POST['description'];
+   $data['location'] = $_POST['location'];
+   $data['salary'] = $_POST['salary'];
+   $data['contact_email'] = $_POST['contact_email'];
+   $data['contact_user'] = $_POST['contact_user'];
+   foreach ($data as $key => $value) {
+      if (empty($value) || $value == 0) {
+         redirect('edit.php', "Please Fill all fields and choose a category", 'error');
+      }else{
+         addslashes($value);
+      }
+   }
+  
+   if ($job->update($job_id, $data)) {
+      redirect('index.php', "Your job has been Updated", 'success');
+   } else {
+      redirect('index.php', "Something Went Wrong", 'error');
+   }
 }
 $template = new Template('templates/job-edit.php');
 $template->job = $job->getJob($job_id);
